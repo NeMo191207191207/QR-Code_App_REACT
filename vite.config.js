05f5637b925759d92e16e38b2d6a9fc04a5d45b2
Alen Troyan <alentroyan@gmail.com>
2025-10-05 16:02:33 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basa: '/QR-Code_App_REACT'
+  basa: '/QR-Code_App_REACT/'
 })
